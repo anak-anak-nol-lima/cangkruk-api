81f68c6b1b3e1585c0d5846192b187a6bf175491
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-07-17 23:54:26 +0700

    feat(auth): implement basic-auth middleware

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
 from controllers.roleplay import router as roleplay_router
+from middlewares.auth import basic_auth_middleware
 
 app = FastAPI()
 
+app.middleware("http")(basic_auth_middleware)
 app.include_router(roleplay_router)
 
 
