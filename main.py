2d7ea3a076ecfd69980f0e6a87edcd93b429b094
Bee Wijaya <beebeewijaya.tech@gmail.com>
2026-07-27 14:06:34 +0700

    feat(logging): adding middleware logging for robust debugging and information

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from fastapi import FastAPI
 from controllers.learning import router as learning_router
 from controllers.roleplay import router as roleplay_router
 from middlewares.auth import basic_auth_middleware
+from middlewares.logging import log_requests
 
 app = FastAPI()
 
+app.middleware("http")(log_requests)
 app.middleware("http")(basic_auth_middleware)
 app.include_router(roleplay_router)
 app.include_router(learning_router)
